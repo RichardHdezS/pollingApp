9b6106424a49129ef28757b4d23e73f84770f49c
RichardHdezS <ricardo123hernandez99@gmail.com>
2022-02-24 22:28:46 -0600

    Se implemento el diseño de registro, home y visualizacion del formulario

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
- 
+
 router.get('/', (req, res)=>{
     res.status(200).render('login');
 });
+
+router.get('/signup', (req, res)=>{
+    res.status(200).render('signUp');
+});
+
+router.get('/home', (req, res)=>{
+    res.status(200).render('home');
+});
+
+router.get('/view', (req, res)=>{
+    res.status(200).render('viewForm');
+});
+
 
 module.exports = router;
